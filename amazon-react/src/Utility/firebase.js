066c57d66d56e66659cc94ef259697cfab52933b
dagimtsegaw <dagatse577@gmail.com>
2024-09-25 17:39:38 +0300

    create-install-firebase

---
+++ amazon-react/src/Utility/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDTRBxWi2jlrcn6JrOEKfRqatfmsTjeZs",
+  authDomain: "clone-68f3e.firebaseapp.com",
+  projectId: "clone-68f3e",
+  storageBucket: "clone-68f3e.appspot.com",
+  messagingSenderId: "746149062523",
+  appId: "1:746149062523:web:7e3861bf921d429dbdbfde",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
