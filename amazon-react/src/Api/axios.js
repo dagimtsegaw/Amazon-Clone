66d3de6a43f91652b6a19d1ff3389c1549444756
dagimtsegaw <dagatse577@gmail.com>
2024-10-01 18:21:37 +0300

    backend deployment

--- amazon-react/src/Api/axios.js
+++ amazon-react/src/Api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-68f3e/us-central1/api",
+  //local firebase function
+  // baseURL: "http://127.0.0.1:5001/clone-68f3e/us-central1/api",
+
+  //amazon on render.com
+  baseURL: "https://amazon-api-deploy-ukse.onrender.com",
 });
 
 export { axiosInstance };
